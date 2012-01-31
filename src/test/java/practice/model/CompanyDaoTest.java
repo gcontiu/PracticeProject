648ffeed3e996bded7a49b1dd9fa54824c12cd9b
Gelu Contiu <anghel.contiu@gmail.com>
2012-02-01 00:34:23 +0200

    Added CompanyDaoTest empty class.

---
+++ src/test/java/practice/model/CompanyDaoTest.java
@@ -0,0 +1,11 @@
+package practice.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gelu
+ * Date: 2/1/12
+ * Time: 12:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CompanyDaoTest {
+}
